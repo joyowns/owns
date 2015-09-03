b1bd53bcdd28bd90c88c6b31411738a8808302ae
Joy Owns <joyowns@gmail.com>
2015-09-03 16:23:55 -0700

    MIXER ADDED

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 var OM = function () {
-  var bpm = 70;         //idk who cares
-  var hitgain = 1;
+  //findtop
   
   var anomaly = [[0,0],[0,0],[0,0],[0,0],[0,0]]; //[type][song][buffer]
   
@@ -78,45 +77,156 @@ var OM = function () {
   var drumsamples=[];    //buffers with rendered drum (pitched) sounds
   var percsamples=[];    //buffers with rendered perc (noise) sounds
   var samplesleft;   //drums left to render
-  var drumvals= [//pitched percussion sounds
-                                    //high,low,bend,fade
-                  [35,18,1,1],      //kickdrum
-                  [42,23,2,2],      //lowtom
-                  [47,30,1,1],      //midtom
-                  [54,35,1/2,1/2],  //hitom
-                  [59,42,1/3,1/3],  //lowsnare
-                  [66,47,1/4,1/4],  //midsnare
-                  [71,54,1/8,1/8],  //hisnare
-                  [78,59,1/16,1/16]  //openhat
-                ];
-  var percvals= [//nonpitched percussion sounds
-                                    //filter,fade
-                  [35,1/2],         //snarenoise
-                  [83,1/16],        //closedhatnoise
-                  [83,1/8]         //openhatnoise
-                ];
+  var anomalyleft = 10;
+
                 
   var vcontext;          //HTML5 canvas
   var acontext;          //WebAudio context
   
+  var bpm = 108;         //idk who cares
   var steptime = 15/bpm; //sixteenth note length
-  
   var nowtime;           //used by parallel hit renderers
   var notes = [];        //lookup table for note frequencies
-  
   var wavnoise;          //buffer with supposedly white noise
   var samplerate;        //samples per second in audio context
 
+  //findfx
+  var hitgain = 2/3;
+  var masterchannel;
+  
+  var delaytime = steptime*3;
+  var delaygain = 1/2;
+  var delay;
+  var feedback;
+  
+  var lowpassnote = 59;
+  var lowpassgain = 4;
+  var lowpass;
+  var lpgain;
+  
+  var highpassnote = 95;
+  var highpassgain = 1;
+  var highpass;
+  var hpgain;
+  
+  
+  var waveform;
+  var wavearray;
+  var wavelength;
+  var wavefft=2048*16;
+  var wavewidth=480.0;
+  var waveheight=40.0*8;
+  
+  var dropexponent = 4;
+  //findmixer
+  var loudsquares = [[1/8,1/8],[1/8,1/8],[1/16,1/24],[1/8,1/24],[0,1/8]];
+  var justdrums = [[1/4,1/4],[0/8,0/8],[0/12,0/16],[0/16,0/6],[0,0/8]];
+  var drumnbass = [[0/8,0/8],[0/8,0/8],[0/12,0/16],[0/16,0/6],[0,2/8]];
+  
+  var anomalygain = loudsquares;
+  var anomalyshift = [
+                        [0,0],    //DRUMBEAT
+                        [0,0],    //DRUMFILL
+                        [-12,36],    //LEFTHAND
+                        [0,24],    //RIGHTHAND
+                        [12,24]     //BASSLINE
+                      ];
+  var drumvals= [//pitched percussion sounds
+                                    //high,low,bend,fade
+                  [47,23,1/16,1/4,2],      //kickdrum
+                  [42,23,2,2,0],      //lowtom
+                  [47,30,1,1,0],      //midtom
+                  [54,35,1/2,1/2,0],  //hitom
+                  [59,42,1/16,1/16,1],  //lowsnare
+                  [66,47,1/4,1/4,0],  //midsnare
+                  [71,54,1/8,1/8,0],  //hisnare
+                  [78,59,1/16,1/16,0]  //openhat
+                ];
+  var percvals= [//nonpitched percussion sounds
+                                    //filter,fade
+                  [35,1/2,1],         //snarenoise
+                  [83,1/16,2],        //closedhatnoise
+                  [83,1/8,2]         //openhatnoise
+                ];
+  
+  
+  var draw = function() {
+    drawvisual = requestAnimationFrame(draw);
+    waveform.getByteTimeDomainData(wavearray);
+    vcontext.fillStyle = 'rgb(0,0,0)';
+    vcontext.fillRect(0,0,480,320);
+    vcontext.fillStyle = 'rgb(192,192,192)';
+    vcontext.fillRect(0,0,wavewidth,waveheight);
+    vcontext.fillStyle = 'rgb(100,100,192)';
+    vcontext.fillRect(0,waveheight/4,
+                    wavewidth*anomalyleft*0.1,
+                    waveheight/2);
+    vcontext.lineWidth = 1;
+    vcontext.strokeStyle = 'rgb(0,0,0)';
+    vcontext.beginPath();
+    var slicewidth = wavewidth / wavelength;
+    var wavex = 0;
+    for(var index=0;index<wavelength;index++){
+      var wavev = wavearray[index] / 128.0;
+      var wavey = wavev * waveheight / 2;
+      if(index===0)vcontext.moveTo(wavex,wavey);
+      else vcontext.lineTo(wavex,wavey);
+    wavex += slicewidth;
+    }
+    vcontext.lineTo(wavewidth,waveheight/2);
+    vcontext.stroke();
+  };
 
   var OM = function () {
-    for(var x=0;x<128;x++)notes[x]=440*Math.pow(2,(x-69)/12);console.log(notes);//generate notes, print to console because why not
-    vcontext = document.getElementById('vcontext');                             //grab canvas reference from HTML
+    for(var x=0;x<256;x++)notes[x]=440*Math.pow(2,(x-69)/12);console.log(notes);//generate notes, print to console because why not
+    chrome.app.window.minWidth = 480;
+    chrome.app.window.minHeight = 320;
+    chrome.app.window.maxWidth = 480;
+    chrome.app.window.maxHeight = 320;
+    vcanvas = document.getElementById('vcanvas');                             //grab canvas reference from HTML
+    vcontext = vcanvas.getContext('2d');
     acontext = new window.AudioContext();                                       //grab WebAudio context from browser
     samplerate = acontext.sampleRate;                                           //grab sample rate from audio context
+    //DELAY UNIT
+    delay=acontext.createDelay(steptime*32);
+    feedback=acontext.createGain();feedback.gain.value=delaygain;
+    delay.delayTime.value=(delaytime);
+    delay.connect(feedback);feedback.connect(delay);
+    delay.connect(acontext.destination);
+    //LOWPASS BOOST
+    lowpass=acontext.createBiquadFilter(); 
+    lowpass.type="lowpass";
+    lowpass.frequency.value = notes[lowpassnote];
+    lpgain =acontext.createGain();
+    lpgain.gain.value = lowpassgain;
+    lowpass.connect(lpgain);lpgain.connect(acontext.destination);
+    //HIGHPASS BOOST
+    highpass=acontext.createBiquadFilter();
+    highpass.type="highpass";
+    highpass.frequency.value = notes[highpassnote];
+    hpgain = acontext.createGain();
+    hpgain.gain.value = highpassgain;
+    highpass.connect(hpgain);hpgain.connect(acontext.destination);
+    //WAVEFORM DISPLAY
+    waveform = acontext.createAnalyser();
+    waveform.fftSize = wavefft;
+    wavelength = waveform.frequencyBinCount;
+    wavearray = new Uint8Array(waveform.fftSize);
+    waveform.getByteTimeDomainData(wavearray);
+    
+    //MASTER CHANNEL
+    masterchannel = acontext.createGain();
+    masterchannel.gain.value = 1;
+    masterchannel.connect(feedback);
+    masterchannel.connect(lowpass);
+    masterchannel.connect(highpass);
+    masterchannel.connect(waveform);
+    masterchannel.connect(acontext.destination);
+    
     noiseclip = acontext.createBuffer(1,samplerate,samplerate);                 //create buffer for noise
     var noisedata = noiseclip.getChannelData(0);                                //grab reference to noise buffer data
     for(x=0;x<noisedata.length;x++)noisedata[x]=Math.random()*2 - 1;            //randomize contents
-    samplesleft = drumvals.length + percvals.length;                            //count how many samples need rendering
+    samplesleft = drumvals.length + percvals.length;                           //count how many samples need rendering
     var drumcallback = function (indexcalled) {                                 //create function for drumsample callbacks
           return function(outputclip){
             drumsamples[indexcalled]=outputclip;
@@ -133,7 +243,7 @@ var OM = function () {
       var drumcontext=new OfflineAudioContext(1,samplerate*drumfade*8,samplerate);    //spawn a buffer generator
       var drumosc=drumcontext.createOscillator();                                     //spawn an oscillator
       var drumgain=drumcontext.createGain();                                          //spawn a volume node
-      drumgain.gain.setValueAtTime(1,0);                                              //set initial volume
+      drumgain.gain.setValueAtTime(drumnode[4],0);                                              //set initial volume
       drumgain.gain.setTargetAtTime(0,0,drumfade);                                    //set fadeout
       drumosc.frequency.value=drumhigh;                                               //set initial pitch
       drumosc.frequency.setTargetAtTime(drumlow,0,drumbend);                          //set final pitch
@@ -150,18 +260,18 @@ var OM = function () {
     for(var percindex=0;percindex<percvals.length;percindex++){                   //create each perc sample
         var percnote=notes[percvals[percindex][0]];                                   //filter pitch
         var percfade=steptime*percvals[percindex][1];                                 //fade time
-        var perccontext=new OfflineAudioContext(1,samplerate*percfade*8,samplerate);  //spawn a buffer generator
+        var perccontext=new OfflineAudioContext(1,samplerate*steptime*12,samplerate);  //spawn a buffer generator
         var percnoise=perccontext.createBufferSource();                               //spawn a buffer node
         percnoise.buffer=noiseclip;                                                   //link noise to buffer node
         var percgain=perccontext.createGain();                                        //spawn a volume node
         var percfilter=perccontext.createBiquadFilter();                              //spawn a filter node
         percfilter.type="highpass";                                                   //set to highpass filter
         percfilter.frequency.value=percnote;                                          //set frequency of filter
-        percgain.gain.setValueAtTime(1,0);                                            //set initial volume
+        percgain.gain.setValueAtTime(percvals[percindex][2],0);                       //set initial volume
         percgain.gain.setTargetAtTime(0,0,percfade);                                  //set fadeout time
         percnoise.connect(percgain);percgain.connect(percfilter);                     //connect the dots
         percfilter.connect(perccontext.destination);                                  //connect to renderer
-        percnoise.start(0);percnoise.stop(percfade*8);                                //set buffer playback
+        percnoise.start(0);percnoise.stop(steptime*8);                                //set buffer playback
         perccontext.startRendering().then(perccallback(percindex));                             //link to perc bank when finished
     }
   };
@@ -171,8 +281,10 @@ var OM = function () {
     var songindex;                                                              //create song index for anomaly bank
     var anomalycallback = function (typecalled,songcalled) {                    //create anomaly linker callback generator
       return function(outputclip){
-        console.log(anomaly);
-        anomaly[typecalled][songcalled]=outputclip;};
+        anomaly[typecalled][songcalled]=outputclip;
+        anomalyleft-= 1;console.log(anomalyleft);
+        if(anomalyleft===0)OM.playscene();
+      };
     };
     for(songindex=0;songindex<2;songindex++){
       var pattern;
@@ -206,7 +318,9 @@ var OM = function () {
                   if(dbnote[0]==8)dbhihat.buffer=percsamples[1];                                  //if closed, set closed hihat
                   else dbhihat.buffer=percsamples[2];                                             //otherwise, set open hihat
                   dbhihat.connect(dbcontext.destination);                                         //connect the dots
-                  if(dbnote[0]==8)dbhihat.loop = true;
+                  if(dbnote[0]==8){
+                      dbhihat.loop=true;dbhihat.loopStart=0;dbhihat.loopEnd=steptime;
+                    }
                   dbhihat.start(steptime*dbnote[1]);
                   dbhihat.stop(steptime*(dbnote[1]+dbnote[2])); //schedule hihat sound
                 } break;
@@ -224,10 +338,10 @@ var OM = function () {
             for(var squareindex=0;squareindex<pattern.length;squareindex++){
               squarenote = pattern[squareindex];
               squareosc = squarecontext.createOscillator();
-              squaregain = squarecontext.createGain();squaregain.gain.value=1/16;
+              squaregain = squarecontext.createGain();squaregain.gain.value=anomalygain[typeindex][songindex];
               squareosc.type = "square";squareosc.connect(squaregain);
               squaregain.connect(squarecontext.destination);
-              squareosc.frequency.value = notes[squarenote[0]];
+              squareosc.frequency.value = notes[squarenote[0]+anomalyshift[typeindex][songindex]];
               squareosc.start(steptime*squarenote[1]);
               squareosc.stop(steptime*(squarenote[1]+squarenote[2]));
             }
@@ -238,42 +352,46 @@ var OM = function () {
       }
     }
     OM.listen();
+    draw();
   };
   
   OM.hit = function(hittime,sourceclip){
     var hitclip=acontext.createBufferSource();
     hitclip.buffer=sourceclip;
     var gain=acontext.createGain();gain.gain.value=hitgain;
-    var delay=acontext.createDelay(steptime*32);
-    var feedback=acontext.createGain();feedback.gain.value=1/3;
-    
-    delay.delayTime.value=(steptime*3);
-    hitclip.connect(delay);
-    delay.connect(feedback);feedback.connect(delay);delay.connect(acontext.destination);
-    hitclip.connect(gain);gain.connect(acontext.destination);
+    hitclip.connect(gain);
+    gain.connect(masterchannel);
     hitclip.start(hittime);
   };
   OM.prevent = function (eX){eX.preventDefault();};
   OM.listen = function () {
     document.body.addEventListener('touchmove',OM.prevent,false);
-    vcontext.addEventListener('mousedown',OM.playscene);
-    vcontext.addEventListener('touchstart',OM.playscene);
+    vcanvas.addEventListener('mousedown',OM.playscene);
+    vcanvas.addEventListener('touchstart',OM.playscene);
   };
   OM.playscene = function (eX){
     nowtime=acontext.currentTime;
-    OM.hit(nowtime,anomaly[0][1]);
+    //OM.hit(nowtime+64*steptime,anomaly[0][0]);
+    //OM.hit(nowtime+96*steptime,anomaly[0][0]);
+    OM.hit(nowtime+112*steptime,anomaly[1][1]);
+    //OM.hit(nowtime+96*steptime,anomaly[1][0]);
     OM.hit(nowtime+64*steptime,anomaly[0][1]);
-    //OM.hit(nowtime+96*steptime,anomaly[1][1]);
-    OM.hit(nowtime,anomaly[2][0]);
-    OM.hit(nowtime+(64*steptime),anomaly[2][0]);
-    OM.hit(nowtime,anomaly[3][1]);
-    OM.hit(nowtime,anomaly[2][0]);
-    OM.hit(nowtime+(64*steptime),anomaly[2][0]);
+    OM.hit(nowtime+96*steptime,anomaly[0][1]);
+    OM.hit(nowtime+32*steptime,anomaly[0][1]);
+    OM.hit(nowtime,anomaly[0][1]);
+    
+    
+    
     OM.hit(nowtime,anomaly[3][1]);
     OM.hit(nowtime,anomaly[3][0]);
-    //OM.hit(nowtime,anomaly[4][1]);
-    //OM.hit(nowtime+(64*steptime),anomaly[4][1]);
-    
+    OM.hit(nowtime,anomaly[2][1]);
+    OM.hit(nowtime,anomaly[2][0]);
+    OM.hit(nowtime+(64*steptime),anomaly[2][1]);
+    OM.hit(nowtime+(64*steptime),anomaly[2][0]);
+    OM.hit(nowtime,anomaly[4][1]);
+    OM.hit(nowtime+(64*steptime),anomaly[4][1]);
+    OM.hit(nowtime,anomaly[4][0]);
+    OM.hit(nowtime+(64*steptime),anomaly[4][0]);
   };
   return OM;
 }();
