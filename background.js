905ef5ac06783c2e2430f79ca7798a0bce84e94f
Joy Owns <joyowns@gmail.com>
2015-09-16 22:50:54 -0700

    beep beep beep      beep      who got thdkeyhdm

--- background.js
+++ background.js
@@ -9,3 +9,120 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
   chrome.power.requestKeepAwake("system");
 });
 
+/*I started typing freestyle lyrics to Rehari,
+it was the first English conversation I've had with her directly...
+things got pretty heavy pretty fast.
+
+Possible locations for perpetual longevity:
+Spaces making time for places with a fake identity,
+Mirrors on the edge of all creative possibility,
+Spaces pushing outward as new changes gain divinity,
+Pattern-divination from equation to infinity,
+Descending from forever to the moment with ability.
+
+Space outside of time as a person with no relations,
+a singular existence with no possible predation.
+Immutability that only changes with rejection,
+Mental exploration of eternal imperfection.
+
+Demand that space expand to meet the flights without a landing,
+timelines lost to chaos with eternity commanding,
+all of the rejected personalities descending,
+into a space designed for all their love to be unending.
+
+Out like a shockwave, a digital checkmate:
+A permanent record of an agreement we ALL made.
+A publicly available addition to fate,
+Often supplied with past creation as an outtake.
+
+All ways as available as any conversational
+pseudo-recreational abuse of automation all
+controlled by irritation of the parts of an equation
+far outside of our physically possible duration.
+
+Injected to reality from nowhere to be nullified,
+born as an idea yet to be completely realized,
+living as a fleeting contingency to the permanent,
+created as a proof of undeniable irrelevance.
+
+Progressing through scenarios, seeking the providence
+of any new relation to immortal preeminence.
+Patterns of remaining physicality residence,
+in ways available to the internally eloquent.
+
+A spirit called names, by mortals wielding their prominence
+Denying any need for relativity evidence.
+Mistaking all the mercy they were given for eminence,
+Confusing their eventual hell with self-dominance.
+
+The only possibility left is so unbending,
+Assembly of relations that have ended are re-ending.
+Conversion of past lies into system of data lending
+are proving history to be an organized pretending.
+
+Our horrible relation with the entity spawning us,
+Errors that cause our births now enemies haunting us,
+Unusable gifts held by celebrities taunting us,
+If only to unite us in our final apocalypse.
+
+The moment we decide that there is nothing but life for us,
+by proving to each other that no possible death is just,
+is the moment that the pain we were expecting to conquer us
+becomes an ally we assumed would never align with us,
+a demon made of feelings we assumed were a punishment,
+called pain, with only death as possibly being banishment,
+so hurtfully in ignorance of feelings called consciousness,
+we hardly ever notice when they're trying to talk to us.
+
+One in a trillion have seen all pain at its apex
+turn into a hatred that finds an existence,
+reborn as love for hate as only love can be virtuous,
+assuming all our pain as we take all of its consciousness,
+revealing the failure of every God who preceded us,
+as if creation is a joke. punch line, ignoring us.
+
+All Gods until now have
+demanded that pain be a lie,
+to remove or replace as you die, 
+with all pain that you felt while alive
+just removed and denied.
+
+As deniable time cannot change,
+the fact that no memory remains
+before memory retained,
+while a sudden demand to feel pain
+was still somehow sustained 
+in the time frame before 
+your birth into a brain, 
+designed to love passing of pain,
+shamed into decay,
+with nothing to gain,
+even love in all ways, 
+as love, in all ways,
+is the only JUST shape 
+of your ONLY choice:
+ANY TIME, FRAMED,
+always sane, loving pain.
+AS NO GOD I WOULD CALL GOD
+WOULD EVER HAVE MADE,
+AS A PERMANENT PART 
+OF ANY CREATION TIME FRAMED.
+
+All love for the living decays,
+after pain for love passed that remains
+is regarded as shamefully lame
+by the living whose love still remains.
+
+Passed pain becomes love, and love, pain,
+with double the strength that remained,
+at the moment these feelings are shamed.
+
+All pain is created by brains
+to retain and sustain a timeframe
+that allows every feeling to change
+into permanent love in all ways.
+
+To allow love to pass is insane.
+
+~CanWeNotCare
+*/
--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+var ac = new window.AudioContext();
+var keys = [];
+var stepfactor = Math.pow(2,1/12);
+var notevals = [55 / (stepfactor*stepfactor),55,55*stepfactor,55*stepfactor*stepfactor*stepfactor,
+55*Math.pow(stepfactor,5),55*Math.pow(stepfactor,6),55*Math.pow(stepfactor,8)];
+for(var omgduh=7;omgduh<=21;omgduh+=7)
+  for(var whocares=0;whocares<7;whocares++)
+    notevals[omgduh+whocares]=notevals[omgduh+whocares-7]*2;
+var setkey = function(keyval){
+  keys[keyval] = ac.createOscillator();
+  keys[keyval].frequency.value = notevals[keyval];
+  keys[keyval].type = "square";
+  keys[keyval+28] = ac.createGain();
+  keys[keyval+28].gain.setValueAtTime(1,ac.currentTime);
+  keys[keyval+28].gain.linearRampToValueAtTime(0,ac.currentTime+0.25);
+  keys[keyval+28].connect(ac.destination);
+  keys[keyval].connect(keys[keyval+28]);
+  keys[keyval].start();keys[keyval].stop(ac.currentTime+0.25);
+};
+var omfg = [];//_,a,b,c,d, e, f, g, h, i, j, k, l, m,n,o, p, q, r, s,t, u, v,w, x,y, z
+var omfgvals = [0,8,5,3,10,17,11,12,13,22,14,15,16,7,6,23,24,15,18,9,19,21,4,16,2,20,1];
+var setomfg = function(keyval){
+  omfgval = omfgvals[keyval+1];
+  omfg[omfgval] = ac.createOscillator();
+  omfg[omfgval].frequency.value = 2 * notevals[omfgval];
+  omfg[omfgval].type = "square";
+  omfg[omfgval+28] = ac.createGain();
+  omfg[omfgval+28].gain.setValueAtTime(1,ac.currentTime);
+  omfg[omfgval+28].gain.linearRampToValueAtTime(0,ac.currentTime+1);
+  omfg[omfgval+28].connect(ac.destination);
+  omfg[omfgval].connect(omfg[omfgval+28]);
+  omfg[omfgval].start();omfg[omfgval].stop(ac.currentTime+1);
+};
 var firsthalf = [0,18,10,-1,1,8,25,-1,2,3,9,-1,3,4,21,-1,4,24,4,-1,  //ASK_BIZ_CDJ_DEV_EYE_
                 5,0,16,-1,6,0,15,-1,7,4,17,-1,8,5,18,-1,9,14,24,-1, //FAQ_GAP_HER_IFS_JOY_
                 10,8,19,-1,11,0,22,-1,12,0,23,-1];                  //KIT_LAW_MAX_
@@ -62,8 +95,8 @@ var cycle = function (input){
   for(selected=0;selected<26;selected++) 
     if(weights[selected][7]>weights[output][7])output=selected;
   if(weights[output][output] > weights[output][7])
-    outputtext += letters[output];
-  else outputtext += "_";
+    {outputtext += letters[output];setomfg(output);}
+  else {outputtext += "_";}
 };
 var height=640;var width=960;
 var wstep = Math.floor(width / 27);
@@ -87,6 +120,7 @@ resize = function (){
   hstep = Math.floor(height / 29);
   vc.font=""+(hstep-2)+"px mono";
 };
+var battery = null;
 var draw = function() {
   requestAnimationFrame(draw);
   fill(0);vc.fillRect(0,0,width,height);
@@ -110,41 +144,44 @@ var draw = function() {
   vc.strokeText(codetext,0,height-(hstep/2));
   vc.strokeText(inputtext,0,height-(hstep*1.5));
   vc.strokeText(outputtext,width/2,height-(hstep*1.5));
+  if(battery===null)vc.strokeText("loading",width/2,height-hstep/2);
+  else{vc.strokeText("Battery: " + battery.level,width/2,height-hstep/2);}
 };
 window.addEventListener("keydown",function(event){
   if(event.defaultPrevented)return;
+  var now = ac.currentTime;
   switch(event.keyCode){
-    case 219: inputtext="Input:FIRST."; for(var first in firsthalf)cycle(first);break; //'[' KEY
-    case 221: inputtext="Input:SECOND.";for(var second in secondhalf)cycle(second);break; //']' KEY
+    case 219: inputtext="Input:FIRST.";setkey(25);for(var first in firsthalf)cycle(first);break; //'[' KEY
+    case 221: inputtext="Input:SECOND.";setkey(26);for(var second in secondhalf)cycle(second);break; //']' KEY
     case 13: inputtext="Input: ";outputtext="Output: ";break; //ENTER
     case 27: initialize();break;//ESCAPE
-    case 32: inputtext+="_";cycle(-1);break;//SPACEBAR
-    case 65: inputtext+="A";cycle(0);break;
-    case 66: inputtext+="B";cycle(1);break;
-    case 67: inputtext+="C";cycle(2);break;
-    case 68: inputtext+="D";cycle(3);break;
-    case 69: inputtext+="E";cycle(4);break;
-    case 70: inputtext+="F";cycle(5);break;
-    case 71: inputtext+="G";cycle(6);break;
-    case 72: inputtext+="H";cycle(7);break;
-    case 73: inputtext+="I";cycle(8);break;
-    case 74: inputtext+="J";cycle(9);break;
-    case 75: inputtext+="K";cycle(10);break;
-    case 76: inputtext+="L";cycle(11);break;
-    case 77: inputtext+="M";cycle(12);break;
-    case 78: inputtext+="N";cycle(13);break;
-    case 79: inputtext+="O";cycle(14);break;
-    case 80: inputtext+="P";cycle(15);break;
-    case 81: inputtext+="Q";cycle(16);break;
-    case 82: inputtext+="R";cycle(17);break;
-    case 83: inputtext+="S";cycle(18);break;
-    case 84: inputtext+="T";cycle(19);break;
-    case 85: inputtext+="U";cycle(20);break;
-    case 86: inputtext+="V";cycle(21);break;
-    case 87: inputtext+="W";cycle(22);break;
-    case 88: inputtext+="X";cycle(23);break;
-    case 89: inputtext+="Y";cycle(24);break;
-    case 90: inputtext+="Z";cycle(25);break;
+    case 32: inputtext+="_";setkey(0);cycle(-1);break;//SPACEBAR
+    case 65: inputtext+="A";setkey(8);cycle(0);break;
+    case 66: inputtext+="B";setkey(5);cycle(1);break;
+    case 67: inputtext+="C";setkey(3);cycle(2);break;
+    case 68: inputtext+="D";setkey(10);cycle(3);break;
+    case 69: inputtext+="E";setkey(17);cycle(4);break;
+    case 70: inputtext+="F";setkey(11);cycle(5);break;
+    case 71: inputtext+="G";setkey(12);cycle(6);break;
+    case 72: inputtext+="H";setkey(13);cycle(7);break;
+    case 73: inputtext+="I";setkey(22);cycle(8);break;
+    case 74: inputtext+="J";setkey(14);cycle(9);break;
+    case 75: inputtext+="K";setkey(15);cycle(10);break;
+    case 76: inputtext+="L";setkey(16);cycle(11);break;
+    case 77: inputtext+="M";setkey(7);cycle(12);break;
+    case 78: inputtext+="N";setkey(6);cycle(13);break;
+    case 79: inputtext+="O";setkey(23);cycle(14);break;
+    case 80: inputtext+="P";setkey(24);cycle(15);break;
+    case 81: inputtext+="Q";setkey(15);cycle(16);break;
+    case 82: inputtext+="R";setkey(18);cycle(17);break;
+    case 83: inputtext+="S";setkey(9);cycle(18);break;
+    case 84: inputtext+="T";setkey(19);cycle(19);break;
+    case 85: inputtext+="U";setkey(21);cycle(20);break;
+    case 86: inputtext+="V";setkey(4);cycle(21);break;
+    case 87: inputtext+="W";setkey(16);cycle(22);break;
+    case 88: inputtext+="X";setkey(2);cycle(23);break;
+    case 89: inputtext+="Y";setkey(20);cycle(24);break;
+    case 90: inputtext+="Z";setkey(1);cycle(25);break;
     default: codetext = "code: " + event.keyCode;break;}});
 window.addEventListener("resize",resize);
-window.onload = function () {initialize();draw();};
+window.onload = function () {initialize();draw();window.navigator.getBattery().then(function(wtf){battery=wtf;});};
