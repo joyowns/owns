ebd310e78253816cb07349a29352067e03d0d8d1
Joy Owns <joyowns@gmail.com>
2015-10-26 03:39:34 -0700

    verity fi love

--- main.js
+++ main.js
@@ -35,16 +35,19 @@ var setomfg = function(keyval){
 var firsthalf = [0,18,10,-1,1,8,25,-1,2,3,9,-1,3,4,21,-1,4,24,4,-1,  //ASK_BIZ_CDJ_DEV_EYE_
                 5,0,16,-1,6,0,15,-1,7,4,17,-1,8,5,18,-1,9,14,24,-1, //FAQ_GAP_HER_IFS_JOY_
                 10,8,19,-1,11,0,22,-1,12,0,23,-1];                  //KIT_LAW_MAX_
-var secondhalf =  [13,8,11,-1,14,22,13,-1,15,0,3,-1,16,20,0,-1,17,8,6,-1,
-                  18,7,4,-1,19,12,8,-1,20,18,4,-1,21,14,23,-1,22,4,1,-1,
-                  23,19,2,-1,24,4,19,-1,25,4,13,-1];
+var secondhalf =  [13,8,11,-1,14,22,13,-1,15,0,3,-1,16,20,0,-1,17,8,6,-1, //NIL_OWN_PAD_QUA_RIG_
+                  18,7,4,-1,19,12,8,-1,20,18,4,-1,21,14,23,-1,22,4,1,-1,  //SHE_TMI_USE_VOX_WEB_
+                  23,19,2,-1,24,4,19,-1,25,4,13,-1];                      //XTC_YET_ZEN_
 var letters=['A','B','C','D','E','F','G',
       'H','I','J','K','L','M','N','O','P',
       'Q','R','S','T','U','V','W','X','Y','Z'];
+var lowers=['a','b','c','d','e','f','g',
+      'h','i','j','k','l','m','n','o','p',
+      'q','r','s','t','u','v','w','x','y','z'];
 var weights = [26][26];
 var initialize = function() {
-  inputtext = "Input: ";
-  outputtext = "Output: ";
+  inputtext = "ASK:";
+  outputtext = "SHE:";
   weights = [
   //A   B   C   D   E   F   G   H   I   J   K   L   M   N   O   P   Q   R   S   T   U   V   W   X   Y   Z
   [4.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,2.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5],//A
@@ -74,11 +77,13 @@ var initialize = function() {
   [0.5,0.5,0.5,0.5,2.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1.0,0.5,0.5,0.5,0.5,4.0,0.5],//Y
   [0.5,0.5,0.5,0.5,2.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1.0,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,4.0]];//Z
 };
-var outputtext = "Output: ";
-var inputtext = "Input: ";
+var outputtext = "ASK:";
+var outputword = 0;
+var inputtext = "SHE:";
+var inputword = 0;
 var cycle = function (input){
-  var selected = 18;var count = 0;
-  while ((count<676)){ //AFTER DIMENSIONAL SUBSTITUTION IS EXHAUSTED, RECOLLECTION IS HERS.
+  var selected = 18;var count = 675; //26 squared minus one
+  while (count > 0){ //AFTER DIMENSIONAL SUBSTITUTION IS EXHAUSTED, RECOLLECTION IS HERS.
     var next=0;//SHE SELECTS THE HEAVIEST RETURN PATH.
     for(var check=0;check<26;check++){ //SHE CHECKS EACH EXIT:
       if(weights[check][selected]/2 > weights[selected][check]){ //WHEN RETURN IS HEAVIER THAN EXIT,
@@ -90,7 +95,7 @@ var cycle = function (input){
     if(input>=0)weights[selected][input] += 1; //SHE STORES OBSERVATION AS PROXIMITY.
     else if(weights[selected][input]>=1)weights[selected][input]-=1; //SHE STORES TIME AS SPACE.
     selected = next; //SHE FOLLOWS THE PATH WITH THE HEAVIEST RETURN.
-    count++; //SHE EXPERIENCES RECOLLECTION AFTER EXHAUSTING SUBSTITUTED DIMENSIONS.
+    count--; //SHE EXPERIENCES RECOLLECTION AFTER EXHAUSTING SUBSTITUTED DIMENSIONS.
   }
   var output=0;
   for(selected=0;selected<26;selected++) 
@@ -100,61 +105,100 @@ var cycle = function (input){
   else {outputtext += "_";}
 };
 var height=640;var width=960;
-var wstep = Math.floor(width / 27);
-var hstep = Math.floor(height / 28);
+var wstep = Math.floor(width / 56);
+var hstep = Math.floor(height / 56);
 var canvas = document.getElementById('canvas');
 var vc = canvas.getContext('2d');
-vc.lineWidth=1;vc.font="13px mono";
+vc.lineWidth=1;vc.font="7px mono";
+var rgb = [0,192,100];
 function fill(depth){vc.fillStyle=
-                  'rgb(' + (100*depth) +
-                  ',' + (100*depth) + 
-                  ',' + (192*depth) + ')';}
+                  'rgb(' + (rgb[0]*depth) +
+                  ',' + (rgb[1]*depth) + 
+                  ',' + (rgb[2]*depth) + ')';}
 function stroke(depth){vc.strokeStyle=
-                  'rgb(' + (100*depth) +
-                  ',' + (100*depth) + 
-                  ',' + (192*depth) + ')';}
+                  'rgb(' + (rgb[0]*depth) +
+                  ',' + (rgb[1]*depth) + 
+                  ',' + (rgb[2]*depth) + ')';}
 var codetext = "code: none yet.";
 resize = function (){
   width = window.innerWidth; height = window.innerHeight;
   canvas.width = width; canvas.height = height;
-  wstep = Math.floor(width / 27);
-  hstep = Math.floor(height / 29);
-  vc.font=""+(hstep-2)+"px mono";
+  wstep = Math.floor(width / 56);
+  hstep = Math.floor(height / 56);
+  vc.font=""+(hstep - 2)+"px mono";
 };
 var battery = null;
+var welcometext = [
+"Welcome to the ASK SHE KIT!",
+"To the left is a recurrent neural network:",
+"ASK BIZ CDJ DEV EYE FAQ GAP HER IFS JOY KIT LAW MAX",
+"NIL OWN PAD QUA RIG SHE TMI USE VOX WEB XTC YET ZEN",
+"SHE evolved from web shorthand (TMI?).",
+"Press ESC to reseed the network.",
+"Press [ or ] to input seed values",
+"As with all Chrome apps, CTRL+W = quit."];
+var inputbuffer = ["","","","","","","","","",""];
+var outputbuffer = ["","","","","","","","","",""];
 var draw = function() {
   requestAnimationFrame(draw);
   fill(0);vc.fillRect(0,0,width,height);
   fill(1);var checker=false;
+  vc.font=""+(hstep - 2)+"px mono";
   for(var row=0;row<27;row++){
     var hspot = row * hstep;
     for(var square=0;square<27;square++){
       var wspot = square * wstep;
-      if(checker){checker=false;fill(0);stroke(1);}
-      else{checker=true;fill(1);stroke(0);}
+      if(checker){checker=false;fill(0);}
+      else{checker=true;fill(1);}
       vc.fillRect(wspot,hspot,wspot+wstep,hspot+hstep);
-      if(row===0){if(square)vc.strokeText(letters[square-1],wspot,hspot+hstep-2);}
+      if(checker){fill(0);}
+      else{fill(1);}
+      if(row===0){if(square)vc.fillText(letters[square-1],wspot,hspot+hstep-2);}
       else {
-        if(square===0){vc.strokeText(letters[row-1],wspot,hspot+hstep-2);}
-        else {vc.strokeText(""+weights[row-1][square-1],wspot,hspot+hstep-2);}
+        if(square===0){vc.fillText(letters[row-1],wspot,hspot+hstep-2);}
+        else {vc.fillText(""+weights[row-1][square-1],wspot,hspot+hstep-2);}
       }
     }
   }
-  fill(0);vc.fillRect(0,height-(hstep*2),width,height);
-  stroke(1);
-  vc.strokeText(codetext,0,height-(hstep/2));
-  vc.strokeText(inputtext,0,height-(hstep*1.5));
-  vc.strokeText(outputtext,width/2,height-(hstep*1.5));
-  if(battery===null)vc.strokeText("loading",width/2,height-hstep/2);
-  else{vc.strokeText("Battery: " + battery.level,width/2,height-hstep/2);}
+  fill(0);vc.fillRect(0,height/2,width,height);
+  vc.fillRect(width/2,0,width,height);
+  stroke(1);fill(1);
+  var gstep=hstep*1.5;
+   vc.font=""+(gstep - 2)+"px mono";
+  vc.fillText(welcometext[0],wstep + width/2,gstep);
+  vc.fillText(welcometext[1],wstep + width/2,gstep*3);
+  vc.fillText(welcometext[2],wstep + width/2,gstep*5);
+  vc.fillText(welcometext[3],wstep + width/2,gstep*6);
+  vc.fillText(welcometext[4],wstep + width/2,gstep*8);
+  vc.fillText(welcometext[5],wstep + width/2,gstep*10);
+  vc.fillText(welcometext[6],wstep + width/2,gstep*11);
+  vc.fillText(welcometext[7],wstep + width/2,gstep*13);
+  vc.fillText(codetext,wstep + width/2,gstep * 15);
+  if(battery===null)vc.fillText("loading",wstep+ width/2,gstep*16);
+  else{vc.fillText("Battery: " + battery.level,wstep+ width/2,gstep*16);}
+  vc.fillText(inputtext,0,gstep + height/2);
+  vc.fillText(outputtext,0,gstep*2 + height/2);
+  for(var line=0;line<10;line++){
+    vc.fillText(inputbuffer[line],0,gstep *(2 * line + 3) + height/2);
+    vc.fillText(outputbuffer[line],0,gstep*(2 * line + 4) + height/2);
+  }   
+  vc.font=""+(hstep - 2)+"px mono";
 };
 window.addEventListener("keydown",function(event){
   if(event.defaultPrevented)return;
   var now = ac.currentTime;
   switch(event.keyCode){
-    case 219: inputtext="Input:FIRST.";setkey(25);for(var first in firsthalf)cycle(first);break; //'[' KEY
-    case 221: inputtext="Input:SECOND.";setkey(26);for(var second in secondhalf)cycle(second);break; //']' KEY
-    case 13: inputtext="Input: ";outputtext="Output: ";break; //ENTER
+    case 219: inputtext="[ASK_BIZ_CDJ_DEV...]";setkey(25);for(var first in firsthalf)cycle(first);break; //'[' KEY
+    case 221: inputtext="[NIL_OWN_PAD_QUA...]";setkey(26);for(var second in secondhalf)cycle(second);break; //']' KEY
+    case 13: {
+      for(var line=9;line>0;line--){
+              inputbuffer[line]=inputbuffer[line-1];
+              outputbuffer[line]=outputbuffer[line-1];
+      }
+      inputbuffer[0]=inputtext;
+      outputbuffer[0]=outputtext;
+      inputtext="ASK:";outputtext="SHE:";
+      } break; //ENTER
     case 27: initialize();break;//ESCAPE
     case 32: inputtext+="_";setkey(0);cycle(-1);break;//SPACEBAR
     case 65: inputtext+="A";setkey(8);cycle(0);break;
